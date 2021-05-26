ac02b4de13f2ab31ee2c88388710e9733760cf5f
Galen Guyer <galen@galenguyer.com>
2021-05-26 00:36:00 -0400

    Add terraform files
    Imported from https://github.com/galenguyer/kube-infra

---
+++ kube-galenguyer-03.tf
@@ -0,0 +1,54 @@
+resource "azurerm_public_ip" "kube-galenguyer-03-ip" {
+ name                         = "kube-galenguyer-03-ip"
+ location                     = "East US"
+ resource_group_name          = azurerm_resource_group.kube.name
+ allocation_method            = "Static"
+ domain_name_label            = "kube-galenguyer-03"
+}
+
+resource "azurerm_network_interface" "kube-galenguyer-03-nic" {
+    name                        = "kube-galenguyer-03-nic"
+    location                    = "eastus"
+    resource_group_name         = azurerm_resource_group.kube.name
+
+    ip_configuration {
+        name                          = "kube-galenguyer-03-nic-config"
+        subnet_id                     = azurerm_subnet.kube-galenguyer-subnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.kube-galenguyer-03-ip.id
+    }
+}
+
+resource "azurerm_network_interface_security_group_association" "kube-galenguyer-03-nic-nsg-association" {
+    network_interface_id      = azurerm_network_interface.kube-galenguyer-03-nic.id
+    network_security_group_id = azurerm_network_security_group.kube-galenguyer-nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "kube-galenguyer-03" {
+    name                  = "kube-galenguyer-03"
+    location              = "eastus"
+    resource_group_name   = azurerm_resource_group.kube.name
+    network_interface_ids = [azurerm_network_interface.kube-galenguyer-03-nic.id]
+    size                  = "Standard_B2s"
+
+    os_disk {
+        name                 = "kube-galenguyer-03-osdisk"
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Debian"
+        offer     = "debian-10"
+        sku       = "10"
+        version   = "latest"
+    }
+
+    admin_username = "chef"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username       = "chef"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+}
---
+++ main.tf
@@ -0,0 +1,153 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "a779aaef-4693-4c3a-bf68-6aa99ffe1acf"
+  features {}
+}
+
+resource "azurerm_resource_group" "kube" {
+  name     = "kube"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "kube-galenguyer-vnet" {
+ name                = "kube-galenguyer-vnet"
+ address_space       = ["10.0.0.0/16"]
+ location            = "eastus"
+ resource_group_name = azurerm_resource_group.kube.name
+}
+
+resource "azurerm_subnet" "kube-galenguyer-subnet" {
+ name                 = "kube-galenguyer-subnet"
+ resource_group_name  = azurerm_resource_group.kube.name
+ virtual_network_name = azurerm_virtual_network.kube-galenguyer-vnet.name
+ address_prefixes       = ["10.0.8.0/24"]
+}
+
+resource "azurerm_network_security_group" "kube-galenguyer-nsg" {
+    name                = "kube-galenguyer-nsg"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.kube.name
+
+    # normal stuff
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "HTTPS"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    # control plane
+    security_rule {
+        name                       = "Kube API Server"
+        priority                   = 1100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "6443"
+        source_address_prefix      = "10.0.8.0/24"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "etcd server client API"
+        priority                   = 1101
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "2379-2380"
+        source_address_prefix      = "10.0.8.0/24"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "kubelet API controller"
+        priority                   = 1102
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "10250"
+        source_address_prefix      = "10.0.8.0/24"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "kube-scheduler"
+        priority                   = 1103
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "10251"
+        source_address_prefix      = "10.0.8.0/24"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "kube-controller-manager"
+        priority                   = 1104
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "10252"
+        source_address_prefix      = "10.0.8.0/24"
+        destination_address_prefix = "*"
+    }
+    # worker node stuff
+    security_rule {
+        name                       = "kubelet API worker"
+        priority                   = 1110
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "10250"
+        source_address_prefix      = "10.0.8.0/24"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "NodePort Services"
+        priority                   = 1111
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "30000-32767"
+        source_address_prefix      = "10.0.8.0/24"
+        destination_address_prefix = "*"
+    }
+}
