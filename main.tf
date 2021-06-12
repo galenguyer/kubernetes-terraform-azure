49b2c61e9c2b78a4ea52d55d3610c1bcc410c746
Galen Guyer <galen@galenguyer.com>
2021-06-12 00:15:45 -0700

    Use a single scaling file

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.26"
+      version = ">=2.26"
     }
   }
 }
@@ -13,141 +12,199 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "k8s" {
-  name     = "k8s"
-  location = var.region
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_region
 }
 
-resource "azurerm_virtual_network" "k8s-vnet" {
- name                = "k8s-vnet"
- address_space       = ["10.0.0.0/16"]
- location            = var.region
- resource_group_name = azurerm_resource_group.k8s.name
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${azurerm_resource_group.rg.name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "k8s-subnet" {
- name                 = "k8s-subnet"
- resource_group_name  = azurerm_resource_group.k8s.name
- virtual_network_name = azurerm_virtual_network.k8s-vnet.name
- address_prefixes       = ["10.0.8.0/24"]
+resource "azurerm_subnet" "subnet" {
+  name                 = "${azurerm_resource_group.rg.name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/16"]
 }
 
-resource "azurerm_network_security_group" "k8s-nsg" {
-    name                = "k8s-nsg"
-    location            = var.region
-    resource_group_name = azurerm_resource_group.k8s.name
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${azurerm_resource_group.rg.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-    # normal stuff
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "HTTP"
-        priority                   = 1002
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "HTTPS"
-        priority                   = 1003
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "443"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-    # control plane
-    security_rule {
-        name                       = "Kube API Server"
-        priority                   = 1100
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "6443"
-        source_address_prefix      = "10.0.8.0/24"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "etcd server client API"
-        priority                   = 1101
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "2379-2380"
-        source_address_prefix      = "10.0.8.0/24"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "k8slet API controller"
-        priority                   = 1102
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "10250"
-        source_address_prefix      = "10.0.8.0/24"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "k8s-scheduler"
-        priority                   = 1103
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "10251"
-        source_address_prefix      = "10.0.8.0/24"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "k8s-controller-manager"
-        priority                   = 1104
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "10252"
-        source_address_prefix      = "10.0.8.0/24"
-        destination_address_prefix = "*"
-    }
-    # worker node stuff
-    security_rule {
-        name                       = "k8slet API worker"
-        priority                   = 1110
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "10250"
-        source_address_prefix      = "10.0.8.0/24"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "NodePort Services"
-        priority                   = 1111
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "30000-32767"
-        source_address_prefix      = "10.0.8.0/24"
-        destination_address_prefix = "*"
-    }
+  # normal stuff
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  # control plane
+  security_rule {
+    name                       = "Kube API Server"
+    priority                   = 1100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "6443"
+    source_address_prefix      = "10.0.8.0/24"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "etcd server client API"
+    priority                   = 1101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "2379-2380"
+    source_address_prefix      = "10.0.8.0/24"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "k8slet API controller"
+    priority                   = 1102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10250"
+    source_address_prefix      = "10.0.8.0/24"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "k8s-scheduler"
+    priority                   = 1103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10251"
+    source_address_prefix      = "10.0.8.0/24"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "k8s-controller-manager"
+    priority                   = 1104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10252"
+    source_address_prefix      = "10.0.8.0/24"
+    destination_address_prefix = "*"
+  }
+  # worker node stuff
+  security_rule {
+    name                       = "k8slet API worker"
+    priority                   = 1110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10250"
+    source_address_prefix      = "10.0.8.0/24"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "NodePort Services"
+    priority                   = 1111
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "30000-32767"
+    source_address_prefix      = "10.0.8.0/24"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nic-subnet-association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "ip" {
+  count               = var.worker_count
+  name                = "${azurerm_resource_group.rg.name}-${count.index}-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  domain_name_label   = "${azurerm_resource_group.rg.name}-${var.unique_id}-${count.index}"
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = var.worker_count
+  name                = "${azurerm_resource_group.rg.name}-${count.index}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${azurerm_resource_group.rg.name}-${count.index}-nic-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = element(azurerm_public_ip.ip.*.id, count.index)
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count                 = var.worker_count
+  name                  = "${azurerm_resource_group.rg.name}-vm-${count.index}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
+  size                  = var.vm_size
+
+  os_disk {
+    name                 = "${azurerm_resource_group.rg.name}-vm-${count.index}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = var.vm_disk_type
+  }
+
+  source_image_reference {
+    publisher = "Debian"
+    offer     = "debian-10"
+    sku       = "10"
+    version   = "latest"
+  }
+
+  admin_username                  = var.username
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,20 +1,40 @@
 variable azure_subscription_id {
-  type = string
+  type    = string
   default = "00000000-0000-0000-0000-000000000000"
 }
-variable region {
-  type = string
+
+variable rg_name {
+  type    = string
+  default = "k8s"
+}
+
+variable rg_region {
+  type    = string
   default = "eastus"
 }
+
 variable vm_size {
-  type = string
-  default = "Standard_B2s"
+  type    = string
+  default = "Standard_B1ls"
 }
+
+// can be one of [Premium_LRS, Standard_LRS, StandardSSD_LRS]
+variable vm_disk_type    {
+  type    = string
+  default = "StandardSSD_LRS"
+}
+
 variable username {
-  type = string
+  type    = string
   default = "user"
 }
-variable uniqueid {
-  type = string
-  default = "abcd"
+
+variable unique_id {
+  type    = string
+  default = "galenguyer"
+}
+
+variable worker_count {
+  type    = number
+  default = 3
 }
